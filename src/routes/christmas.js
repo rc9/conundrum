e0e9a659a7e99747736495b4c6e8f87291d774d1
Riley Chang <me@rileyschang.com>
2018-12-24 12:14:24 -1000

    update font

--- src/routes/christmas.js
+++ src/routes/christmas.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from '../header.js';
 import Footer from '../footer.js';
 import '../styles/index.scss';
-import snowy from '../images/hohoho.gif';
+import snowy from '../images/snowy.gif';
 
 export default class Christmas extends Component {
   render() {
