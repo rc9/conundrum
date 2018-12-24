5da1e882b64d6e21da3bfc3fea7b082e5d9c0700
Riley Chang <me@rileyschang.com>
2018-12-24 09:02:38 -1000

    christmas route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './routes/start.js';
 import Signup from './routes/signup.js';
+import Christmas from './routes/christmas.js';
 
 class App extends React.Component {
 
@@ -10,6 +11,7 @@ class App extends React.Component {
       <Router>
           <Switch>
             <Route path="/gettingstarted" component={Home} />
+            <Route path="/hohoho" component={Christmas} />
             <Route path="/" component={Signup} />
           </Switch>
       </Router>
---
+++ src/routes/christmas.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Header from '../header.js';
+import Footer from '../footer.js';
+import '../styles/index.scss';
+import snowy from '../images/hohoho.gif';
+
+export default class Christmas extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="ho-ho-ho">
+            <img src={snowy} alt="Winter is filled with cupidly dancers,
+Holiday movies, toys, and gay prancers. 
+Vixens feign joy to trap dashers’ asses
+Coming like comets down donner passes.
+All this merriment and cheer but alas
+But who is missing from this snowy class…
+"/>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
